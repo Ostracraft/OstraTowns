b5e81ad65f3c1a5e95d4dd62961e87b170c95708
Skylyxx <skylyxxfr@gmail.com>
2021-06-01 17:58:55 +0200

    ⚡️ Change command registering system

--- src/main/java/fr/ostracraft/towns/OstraTowns.java
+++ src/main/java/fr/ostracraft/towns/OstraTowns.java
@@ -1,39 +1,24 @@
 package fr.ostracraft.towns;
 
+import fr.bakaaless.api.command.CommandManager;
+import fr.bakaaless.api.command.CommandRunner;
 import fr.ostracraft.towns.types.Resident;
-import fr.ostracraft.towns.types.TownsCommand;
 import fr.ostracraft.towns.utils.Config;
 import fr.ostracraft.towns.utils.Messages;
 import fr.ostracraft.towns.utils.ReflectionUtil;
-import fr.ostracraft.towns.utils.annotations.CommandProperties;
 import org.bukkit.Bukkit;
-import org.bukkit.command.PluginCommand;
-import org.bukkit.command.SimpleCommandMap;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.List;
 
 public class OstraTowns extends JavaPlugin {
 
-    private static SimpleCommandMap commandMap;
-
-    static {
-        try {
-            final Field bukkitCommandMapField;
-            bukkitCommandMapField = Bukkit.getServer().getClass().getDeclaredField("commandMap");
-            bukkitCommandMapField.setAccessible(true);
-            commandMap = (SimpleCommandMap) bukkitCommandMapField.get(Bukkit.getServer());
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            e.printStackTrace();
-        }
-    }
+    private CommandManager commandManager;
 
     @Override
     @SuppressWarnings("unused")
@@ -53,37 +38,11 @@ public class OstraTowns extends JavaPlugin {
     }
 
     private void registerCommands() {
+        this.commandManager = new CommandManager(this);
         List<Class<?>> classes = ReflectionUtil.getClasses(getClass().getPackageName() + ".commands");
         for (Class<?> clazz : classes) {
-            if (!TownsCommand.class.isAssignableFrom(clazz))
-                continue;
-            try {
-                TownsCommand command = (TownsCommand) clazz.getConstructor().newInstance();
-                CommandProperties commandProperties = clazz.getAnnotation(CommandProperties.class);
-
-                String name = commandProperties.name();
-                String description = commandProperties.description();
-                String permission = commandProperties.permission();
-                String usage = commandProperties.usage();
-
-                PluginCommand pluginCommand = getServer().getPluginCommand(name);
-                if (pluginCommand == null) {
-                    final Constructor<PluginCommand> constructor = PluginCommand.class.getDeclaredConstructor(String.class, Plugin.class);
-                    constructor.setAccessible(true);
-                    pluginCommand = constructor.newInstance(name, this);
-                    pluginCommand.setLabel(name);
-                    pluginCommand.setDescription(description.trim().equalsIgnoreCase("") ? "/" + name : usage);
-                    pluginCommand.setPermission(permission);
-                    pluginCommand.setUsage(usage.trim().equalsIgnoreCase("") ? "/" + name : usage);
-                    pluginCommand.setAliases(command.getAliases());
-                    pluginCommand.setPermissionMessage(Messages.NO_PERM.format());
-                    commandMap.register(getName(), pluginCommand);
-                }
-                pluginCommand.setExecutor(command);
-                pluginCommand.setTabCompleter(command);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            if(CommandRunner.class.isAssignableFrom(clazz))
+                this.commandManager.registerRunners((Class<? extends CommandRunner>) clazz);
         }
     }
 
---
+++ src/main/java/fr/ostracraft/towns/commands/TownCommand.java
@@ -0,0 +1,5 @@
+package fr.ostracraft.towns.commands;
+
+public class TownCommand {
+
+}
